4d3eef7a61ca3d07861dc00df222ea8497421877
Ngan Ting Fung <moses.ngan@preface.education>
2022-11-23 02:06:13 +0800

    feat: add sort to GET campaigns

--- server/src/repositories/campaigns.repository.js
+++ server/src/repositories/campaigns.repository.js
@@ -8,8 +8,25 @@ class CampaignsRepository {
   async getCampaigns() {
     try {
       const campaigns = await this.db.Campaign.findAll({ include: 'candidates' });
-      // TODO: add sorting rules
-      return campaigns;
+
+      // sorting start
+      let sortedCampaigns = [];
+
+      const onGoingCampaigns = campaigns.filter(campaign => campaign.startedAt < new Date() && campaign.endedAt > new Date());
+      const endedCampaigns = campaigns.filter(campaign => campaign.endedAt < new Date());
+
+      // sort by total vote count for ongoing campaigns
+      const sortedOngoingCampaigns = onGoingCampaigns.sort((a, b) =>  {
+        return a.candidates.reduce((totalVote, candidate) => totalVote + candidate.voteCount, 0) > b.candidates.reduce((totalVote, candidate) => totalVote + candidate.voteCount, 0);
+      });
+      // sort by most recent end time for ended campaigns
+      const sortedEndedCampaigns = endedCampaigns.sort((a, b) =>  a.endedAt < b.endedAt );
+
+      // ongoing come first, then ended
+      sortedCampaigns = sortedOngoingCampaigns.concat(sortedEndedCampaigns);
+      // sorting end
+
+      return sortedCampaigns;
     } catch (err) {
       console.log(err);
       return [];
