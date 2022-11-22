dab8ab546112ce57e27fefebf965e3c16af16097
Ngan Ting Fung <moses.ngan@preface.education>
2022-11-22 20:51:17 +0800

    feat: added docker setup to the project

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -13,7 +14,12 @@ app.use(cors({
 app.use(morgan('combined'));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api', api);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
